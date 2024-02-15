b4211e084007398a2d75afeb4a576dd9ad2f8da7
shrey0902 <shreyagarwal2002@gmail.com>
2024-02-15 21:09:46 +0530

    Quiz App made using flutter

---
+++ lib/data/questions.dart
@@ -0,0 +1,94 @@
+import 'package:quiz_app/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'Which planet is known as the "Red Planet"?',
+    [
+      'Mars',
+      'Venus',
+      'Jupiter',
+      'Saturn',
+    ],
+  ),
+  QuizQuestion(
+    'What is the largest mammal in the world?',
+    [
+      'Blue Whale',
+      'Elephant',
+      'Giraffe',
+      'Hippopotamus',
+    ],
+  ),
+  QuizQuestion(
+    'In which year did the Titanic sink?',
+    [
+      '1912',
+      '1905',
+      '1920',
+      '1899',
+    ],
+  ),
+  QuizQuestion(
+    'Who is the author of "To Kill a Mockingbird"?',
+    [
+      'Harper Lee',
+      'J.K. Rowling',
+      'George Orwell',
+      'Mark Twain',
+    ],
+  ),
+  QuizQuestion(
+    'What is the capital city of Japan?',
+    [
+      'Tokyo',
+      'Beijing',
+      'Seoul',
+      'Bangkok',
+    ],
+  ),
+  QuizQuestion(
+    'Who painted the Mona Lisa?',
+    [
+      'Leonardo da Vinci',
+      'Vincent van Gogh',
+      'Pablo Picasso',
+      'Claude Monet',
+    ],
+  ),
+  QuizQuestion(
+    'What is the currency of Brazil?',
+    [
+      'Brazilian Real',
+      'Peso',
+      'Euro',
+      'Yen',
+    ],
+  ),
+  QuizQuestion(
+    'Which element has the chemical symbol "O" and atomic number 8?',
+    [
+      'Oxygen',
+      'Gold',
+      'Carbon',
+      'Iron',
+    ],
+  ),
+  QuizQuestion(
+    'What is the longest river in the world?',
+    [
+      'Amazon River',
+      'Nile River',
+      'Yangtze River',
+      'Mississippi River',
+    ],
+  ),
+  QuizQuestion(
+    'Who wrote "Romeo and Juliet"?',
+    [
+      'William Shakespeare',
+      'Jane Austen',
+      'Charles Dickens',
+      'Homer',
+    ],
+  ),
+];
---
+++ lib/quiz.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/results_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  List<String> selectedAnswers = [];
+  var activeScreen = 'start-screen';
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  void chooseAnswers(String answer) {
+    selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen = 'results-screen';
+      });
+    }
+  }
+
+  void restartQuiz() {
+    setState(() {
+      selectedAnswers = [];
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  @override
+  Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'questions-screen') {
+      screenWidget = QuestionsScreen(
+        onSelectAnswer: chooseAnswers,
+      );
+    }
+    if (activeScreen == 'results-screen') {
+      screenWidget = ResultsScreen(
+        chosenAnswers: selectedAnswers,
+        onRestart: restartQuiz,
+      );
+    }
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 11, 180, 76),
+                Color.fromARGB(255, 128, 18, 201),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: screenWidget,
+        ),
+      ),
+    );
+  }
+}
